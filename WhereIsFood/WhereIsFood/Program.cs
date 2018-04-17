eb1773e1333c8f3135e7cd06721cd16db02b97e0
ArchieKast <kastyushkin95@mail.ru>
2018-04-17 14:55:18 +0300

    Test connection #2.
    Test connection to the DB.

---
+++ WhereIsFood/WhereIsFood/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using System.Data.Common;
+
+namespace WhereIsFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;Port=5432;User=postgres;Password=root;Database=course_project;");
+            conn.Open();
+            NpgsqlCommand command = new NpgsqlCommand("select * from purchases", conn);
+            NpgsqlDataReader reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                foreach (DbDataRecord record in reader)
+                    Console.WriteLine(record["purchase_id"]);
+            }
+            else
+                Console.WriteLine("Запрос не вернул строк");
+            Console.ReadKey();
+        }
+    }
+}
